1d2929e5213e3843c246d0333f641a938123b149
Aqil Contractor <aqilcm@gmail.com>
2022-12-15 03:07:49 -0500

    made a thing render in the middle of the screen after messing around with async for a while

--- macroproj/src/main.rs
+++ macroproj/src/main.rs
@@ -1,12 +1,16 @@
 use macroquad::prelude::*;
 
 pub mod objs;
+use objs::base::*;
+use objs::load::*;
 
 #[macroquad::main("Hi")]
 async fn main() {
+	let mut objs = load_all();
   loop {
     clear_background(BLACK);
     
+		objs[0].draw(0.0, 0.0).await;
     next_frame().await;
   }
 }
--- macroproj/src/objs/base.rs
+++ macroproj/src/objs/base.rs
@@ -1,38 +1,56 @@
 use macroquad::prelude::*;
-use futures::try_join;
+use futures::{
+	future::join_all,
+};
 
 #[derive(Clone, Copy)]
 pub enum Direction {
-  N = 0, W = 1, E = 2, S = 3
+  E = 0, W = 1, N = 2, S = 3
 }
 
 pub struct Object {
   pub img: String,
   pub dir: Direction,
   pub loaded: bool,
-  pub textures: [Texture2D; 4]
+  pub textures: [Option<Texture2D>; 4]
 }
 
 impl Object {
-  pub async fn load(name: &str) -> Self {
-    let textures = try_join!(
-      load_texture(name),
-      load_texture(name),
-      load_texture(name),
-      load_texture(name),
-    ).unwrap();
+  pub fn new(name: &str) -> Self {
     return Object {
       dir: Direction::N,
       img: name.to_string(),
       loaded: true,
-      textures: [textures.0, textures.1, textures.2, textures.3]
+      textures: [None; 4]
     }
   }
-  pub fn draw(&self, x: f32, y: f32) {
-      draw_texture(self.textures[self.dir], x, y, WHITE);
+  pub async fn draw(&mut self, x: f32, y: f32) {
+		if self.textures[self.dir as usize].is_none() { self.load_side(self.dir).await }
+    draw_texture(self.textures[self.dir as usize].unwrap(), x, y, WHITE);
   }
   pub fn up(&mut self) { self.dir = Direction::N; }
   pub fn down(&mut self) { self.dir = Direction::S; }
   pub fn left(&mut self) { self.dir = Direction::E; }
   pub fn right(&mut self) { self.dir = Direction::W; }
+
+	pub async fn load_side(&mut self, dir: Direction) {
+		match dir {
+			Direction::E => {
+				self.textures[0] = load_texture(&format!("./res/imgs/{}_E.png", &self.img)).await.ok()
+      },
+			Direction::W => {
+				self.textures[1] = load_texture(&format!("./res/imgs/{}_W.png", &self.img)).await.ok()
+      },
+			Direction::N => {
+				self.textures[2] = load_texture(&format!("./res/imgs/{}_N.png", &self.img)).await.ok()
+      },
+			Direction::S => {
+				self.textures[3] = load_texture(&format!("./res/imgs/{}_S.png", &self.img)).await.ok()
+      },
+    }
+  }
 }
+
+//pub async fn load_objs(list: Vec<&str>) -> Vec<Object> {
+//	join_all(list.iter().map(|s| Object::load(s))).await
+//}

f9d63869a4059c043c1351e6aeaf99d2479f0d78
Aqil Contractor <aqilcm@gmail.com>
2022-12-30 18:23:32 -0500

    Updated packing into a bin folder

---
+++ cdungeon/package/maps/Door.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Door" tilewidth="16" tileheight="16" tilecount="48" columns="8" backgroundcolor="#000000">
+ <image source="C:/Users/Austin Bilbro/Downloads/DawnLike/Objects/Door0.png" width="128" height="96"/>
+</tileset>
---
+++ cdungeon/package/maps/Map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Map" tilewidth="16" tileheight="16" tilecount="180" columns="12">
+ <image source="C:/Users/Austin Bilbro/Downloads/DawnLike/Objects/Map0.png" trans="000000" width="192" height="240"/>
+</tileset>
